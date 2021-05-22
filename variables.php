8a4fa7af2cb8285ae82c4aad6bb45ce375c03f7f
Geovany Alonso Pinilla <galonso6@misena.edu.co>
2021-05-22 02:53:29 -0500

    Adicionando archivos

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$miVariable = 'Esta es mi primera variable';
+
+//echo$miVariable;//
+var_dump($miVariable);
+$miNumero=10;
+$miDecimal=10.5;
+var_dump($miNumero);
+var_dump($miDecimal);
+?>
