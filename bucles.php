b89ac82b486813197f9596fd82c742757c3c8fab
Geovany Alonso Pinilla <galonso6@misena.edu.co>
2021-05-27 14:30:56 -0500

    Se adicionan lineas del bucle while, if

--- bucles.php
+++ bucles.php
@@ -2,41 +2,51 @@
     //inicio-termina-//
 $limit = 20 ;
 
-for ($i=0; $i <= $limit; $i++) { 
-  echo 'El conteo va en ', $i . '<br>' ;
+for ($i=0; $i<=$limit ; $i++) { 
+    print 'El conteo va en ' . $i . '<br> ';
 }
+echo '<br>';
 
+echo 'while multiplica 5 *2 hasta el limite que es 20, 5*2=10 luego sigue el ciclo 10*2=20  y luego 20*2=40 y ahi termina el cliclo';
+echo '<br>';
+echo '<br>';
 
-$a = 5;                 
-while ($a <= $limit) {
-    $a = $a * 2;
+ $a = 5;
+ while ($a <= $limit) {
+     $a = $a * 2;
+     echo "$a es $a <br>";
 
-    echo "a es $a <br>";
-}
+ }
 
 //mostrar los numeros pares desde el 0 hasta el 100//
+ for ($i=1; $i <=100 ; $i+=1) { 
+     if ($i % 2 ==0) {
+         echo"Este numero es par . $i <br>";
+     }
+ }
+ //mostrar los numeros pares e impares desde el 0 hasta el 100//
+ echo "<br>";
+$inc = 1;
+while ($inc <= 30) { 
+    $num = "El numero $inc es ";
 
+    echo "<br>";
 
-
-
-$motos = [
-'Zusuki',
-'Akt',
-'CBR 250R',
-];
-foreach ($motos as $key => $value) {
-    echo "llave: $key y valor: $value <br>";
+    if ($inc % 2 === 0) {
+        echo "$num  par";
+       
+    } else {
+        echo "$num  impar";
+    }
+    echo "<br>";
+$inc ++;
 }
 
-var_dump($motos);
-echo "<br>";
 
-$autos = [
-    'Mazda'=> [
-        'CX5';
-        '323';
-        'CX7';
-        '2';
-    ];
 
-];
+
+
+
+
+
+?>      
