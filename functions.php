30c9d2074d959a211024044cb02feaaf0f76eed0
Geovany Alonso Pinilla <galonso6@misena.edu.co>
2021-05-27 20:14:58 -0500

    calculadora y funciones

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+function concatenar($txt1, $txt2){
+    return $txt1 . ' ' . $txt2;
+
+}
+echo concatenar('Hola', 'Mundo');
+
+echo '<br>';
+//calculadora en php//
+
+function suma(int $num1, $num2){
+    return $num1  +  $num2;
+}
+
+function resta(int $num1, $num2){
+    return $num1  -  $num2;
+}
+
+function multiplica(int $num1, $num2){
+    return $num1  *  $num2;
+}
+
+function divide(int $num1, $num2){
+    return $num1  /  $num2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
