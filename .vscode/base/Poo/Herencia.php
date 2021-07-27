e9276c3cd7d0a88df58fb65fdf35f0f814c5cce9
Geovany Alonso Pinilla <galonso6@misena.edu.co>
2021-07-27 18:47:29 -0500

    envios por get post require

--- .vscode/base/Poo/Herencia.php
+++ .vscode/base/Poo/Herencia.php
@@ -2,13 +2,13 @@
 
 require_once('Employee.php');
 
-$employee = new Employee(60);
+$employee = new Employee(100);
 var_dump($employee->getUserId());
 var_dump($employee->getPassword());
 
 echo '<br><br>';
 
-$employee->setName('Pacho');
+$employee->setName('Geovany');
 $employee->setLastname('Gonzalez');
 
 var_dump($employee->getFullName());
--- .vscode/base/liquidacionActions.php
+++ .vscode/base/liquidacionActions.php
@@ -1,3 +1,31 @@
 <?php
-var_dump($_REQUEST);
+require_once('../base/functions.php');
+
+$salario = $_REQUEST['salario'] ?? '';
+$salario = intval($salario);
+$cedula = $_REQUEST['cedula'] ?? '';
+$salud = $pension = $arl = $fps = 0;
+$errorSalario = '';
+
+if ($salario && $cedula) {
+  $salud = deducible($salario, 'salud');
+  $pension = deducible($salario, 'pension');
+  $arl = deducible($salario, 'arl');
+  $fps = deducible($salario, 'fps');
+}
+
+if (isset($_REQUEST['salario']) && !$_REQUEST['salario']) {
+  $errorSalario = 'Debe ingresar un salario';
+}
+
+if (isset($_REQUEST['salario']) && $_REQUEST['salario'] && !$salario) {
+  $errorSalario = 'Debe ingresar un número mayor a 0';
+}
+
+$aPathOrigin = explode('?', $_SERVER['HTTP_REFERER']);
+$pathOrigin = $aPathOrigin[0];
+
+var_dump($pathOrigin);
+header("Location: $pathOrigin?salud=$salud");
+?>
 ?>
